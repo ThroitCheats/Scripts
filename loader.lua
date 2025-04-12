8eca8ab80b7d7d3d502c145b0ede27f59d3463ea
ThroitCheats <71610228+ThroitCheats@users.noreply.github.com>
2025-04-12 11:04:03 -0400

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -9,12 +9,12 @@ local function loadTrident()
     local fastflag = getfflag and getfflag('DebugRunParallelLuaOnMainThread');
     local transfer = [[getgenv().script_key = "]]..getgenv().script_key..'"\n'
 
-    if run_on_actor and getactors and (getactors()[1] or game.Players.LocalPlayer.PlayerScripts:FindFirstChildWhichIsA("Actor")) then
+    if run_on_actor and ((getactors and getactors()[1]) or game.Players.LocalPlayer.PlayerScripts:FindFirstChildWhichIsA("Actor")) then
         run_on_actor(getactors()[1] or game.Players.LocalPlayer.PlayerScripts:FindFirstChildWhichIsA("Actor"),transfer..[[
             loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/b0de7c4d81201b8837309655e4de6db7.lua"))()
             game.CoreGui.levelUpLoader:Destroy()
         ]])
-    elseif tostring(string.lower(fastflag)) == "true" then 
+    elseif tostring(string.lower(fastflag or "false")) == "true" then 
 
         script_key = getgenv().script_key
         loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/b0de7c4d81201b8837309655e4de6db7.lua"))()
