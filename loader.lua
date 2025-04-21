42be7cf48b9464c1e9186b6d56e75f38cd28ac3a
ThroitCheats <71610228+ThroitCheats@users.noreply.github.com>
2025-04-21 15:37:15 -0400

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -8,7 +8,8 @@ local queueteleport = (syn and syn.queue_on_teleport) or queue_on_teleport or (f
 local function loadTrident()
     local fastflag = getfflag and getfflag('DebugRunParallelLuaOnMainThread');
     local transfer = [[getgenv().script_key = "]]..getgenv().script_key..'"\n'
-
+    local id = identifyexecutor and identifyexecutor()
+    if id == "Potassium" then run_on_actor = nil end
     if run_on_actor and ((getactors and getactors()[1]) or game.Players.LocalPlayer.PlayerScripts:FindFirstChildWhichIsA("Actor")) then
         run_on_actor(getactors()[1] or game.Players.LocalPlayer.PlayerScripts:FindFirstChildWhichIsA("Actor"),transfer..[[
             loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/b0de7c4d81201b8837309655e4de6db7.lua"))()
